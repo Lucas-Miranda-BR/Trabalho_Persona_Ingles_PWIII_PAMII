76746f81540c396971d2845233627169e82e44b6
Lucas <lucasmirandabr0508@gmail.com>
2026-06-25 20:46:29 -0300

    Validação removida do saveProduto
    Causava erros

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -98,34 +98,6 @@ function deleteProduto(string $produto_id) {
             $dados->merge([
                 'valor' => str_replace(',', '.', $dados->valor),
             ]); 
-            $validator = Validator::make(
-                $dados->all(),
-                  [
-                      'nome' => 'required|min:3|max:255',
-                      'descricao' => 'required|max:255',
-                      'valor' => 'required|numeric|min:0',
-                      'estoque' => 'required|integer'
-                      
-                  ],
-                  [
-                      'nome.required' => 'O campo nome é obrigatório.',
-                      'nome.min' => 'O campo nome deve conter no mínimo 3 caracteres.',
-                      'nome.max' => 'O campo nome deve conter no máximo 255 caracteres.',
-                      'descricao.required' => 'O campo descrição é obrigatório.',
-                      'descricao.max' => 'o campo descrição deve conter no máximo 255 caracteres.',
-                      'valor.required' => 'O campo valor é obrigatório.',
-                      'valor.numeric' => 'O campo valor deve ser um decimal.',
-                      'estoque.required' => 'O campo estoque é obrigatório.',
-                      'estoque.integer' => 'O campo estoque deve ser um número.',
-                  ]
-          );
-    
-          if ($validator->fails()) {
-              return redirect()
-                  ->route('dashboard.produto.update')
-                  ->withErrors($validator)
-                  ->withInput();
-          }
           $produto = \App\Models\Produto::findOrFail($dados->id);
           $produto->update($dados->all());
     
